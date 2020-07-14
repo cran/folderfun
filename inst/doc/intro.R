172121393cf865d42deff24ae0027cbc535e476e
Nathan C. Sheffield <nathan@code.databio.org>
2020-07-14 17:20:03 +0000

    version 0.1.3

--- inst/doc/intro.R
+++ inst/doc/intro.R
@@ -1,8 +1,8 @@
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 # These settings make the vignette prettier
 knitr::opts_chunk$set(results="hold", collapse=TRUE)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  # Load our data:
 #  input1 = read.table("/long/and/annoying/hard/coded/path/data.txt")
 #  input2 = read.table("/long/and/annoying/hard/coded/path/data2.txt")
@@ -15,7 +15,7 @@ knitr::opts_chunk$set(results="hold", collapse=TRUE)
 #  write.table("/different/long/annoying/hard/coded/path/result.txt", output1)
 #  write.table("/different/long/annoying/hard/coded/path/result2.txt", output2)
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  inputDir = "/long/and/annoying/hard/coded/path"
 #  outputDir = "/different/long/annoying/hard/coded/path"
 #  
@@ -29,16 +29,16 @@ knitr::opts_chunk$set(results="hold", collapse=TRUE)
 #  write.table(file.path(outputDir, "result.txt"))
 #  write.table(file.path(outputDir, "result2.txt"))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library(folderfun)
 setff("In", "/long/and/annoying/hard/coded/path/")
 setff("Out", "/different/long/annoying/hard/coded/path/")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 ffIn("data.txt")
 ffOut("result.txt")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  input1 = read.table(ffIn("data.txt"))
 #  input2 = read.table(ffIn("data2.txt"))
 #  output1 = processData(input)
@@ -49,50 +49,50 @@ ffOut("result.txt")
 #  write.table(ffOut("result.txt"))
 #  write.table(ffOut("result2.txt"))
 
-## ----include=FALSE-------------------------------------------------------
+## ----include=FALSE------------------------------------------------------------
 Sys.setenv(INDIR="/long/and/annoying/hard/coded/path/")
 Sys.setenv(OUTDIR="/different/long/annoying/hard/coded/path/")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 options(INDIR="/long/and/annoying/path/to/hard/coded/file/")
 options(OUTDIR="/different/long/annoying/hard/coded/path/")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 setff("In", Sys.getenv("INDIR"))
 setff("Out", Sys.getenv("OUTDIR"))
 
 ffIn()
 ffIn("data.txt")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 setff("In", getOption("INDIR"))
 setff("Out", getOption("OUTDIR"))
 
 ffIn()
 ffIn("data.txt")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  setff("In", pathVar="INDIR")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  setff("Indir")
 
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 # We should populate this variable to use it in the next section
 options(DATA="/long/and/annoying/path/to/hard/coded/file/")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 projectName="myproject"
 setff("Data", pathVar="DATA", postpend=projectName)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 projectName="myproject"
 setff("Data", postpend=projectName)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 listff()
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  name = "DUMMYTESTVAR"
 #  value = "test_value"
 #  
